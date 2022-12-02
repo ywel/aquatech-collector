2c1c14af29ec36cdf6bdde32d918030673c3afe6
Lewis Maina <munenelewis77@gmail.com>
2022-12-02 16:24:07 +0300

    commit uno

--- src/main/java/io/aquatech/dto/ServiceData.java
+++ src/main/java/io/aquatech/dto/ServiceData.java
@@ -5,7 +5,7 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-
+//test
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
     "timeOfReading",
